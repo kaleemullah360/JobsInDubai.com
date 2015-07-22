a2625c6042c64af4e87e385d815571084f4b23c8
kaleemullah360 <kaleemullah360@live.com>
2015-07-22 14:38:26 +0500

    Final release

---
+++ JobsInDubai.com/JobsInDubai.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace JobsInDubai.com
+{
+    partial class JobsInDubai
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nextPage_label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.url_label = new System.Windows.Forms.Label();
+            this.url_textBox = new System.Windows.Forms.TextBox();
+            this.open_btn = new System.Windows.Forms.Button();
+            this.quit_btn = new System.Windows.Forms.Button();
+            this.start_Btn = new System.Windows.Forms.Button();
+            this.result_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.page_no_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nextPage_label
+            // 
+            this.nextPage_label.AutoSize = true;
+            this.nextPage_label.Location = new System.Drawing.Point(636, 441);
+            this.nextPage_label.Name = "nextPage_label";
+            this.nextPage_label.Size = new System.Drawing.Size(21, 13);
+            this.nextPage_label.TabIndex = 20;
+            this.nextPage_label.Text = "No";
+            this.nextPage_label.Click += new System.EventHandler(this.nextPage_label_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(520, 441);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Next page available ?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Total Pages:";
+            // 
+            // url_label
+            // 
+            this.url_label.AutoSize = true;
+            this.url_label.Location = new System.Drawing.Point(1, 441);
+            this.url_label.Name = "url_label";
+            this.url_label.Size = new System.Drawing.Size(23, 13);
+            this.url_label.TabIndex = 16;
+            this.url_label.Text = "Url:";
+            // 
+            // url_textBox
+            // 
+            this.url_textBox.Location = new System.Drawing.Point(30, 438);
+            this.url_textBox.Name = "url_textBox";
+            this.url_textBox.Size = new System.Drawing.Size(194, 20);
+            this.url_textBox.TabIndex = 15;
+            this.url_textBox.Text = "https://www.jobsindubai.com";
+            // 
+            // open_btn
+            // 
+            this.open_btn.Location = new System.Drawing.Point(719, 436);
+            this.open_btn.Name = "open_btn";
+            this.open_btn.Size = new System.Drawing.Size(75, 23);
+            this.open_btn.TabIndex = 14;
+            this.open_btn.Text = "&Open";
+            this.open_btn.UseVisualStyleBackColor = true;
+            this.open_btn.Click += new System.EventHandler(this.open_btn_Click);
+            // 
+            // quit_btn
+            // 
+            this.quit_btn.Location = new System.Drawing.Point(881, 436);
+            this.quit_btn.Name = "quit_btn";
+            this.quit_btn.Size = new System.Drawing.Size(75, 23);
+            this.quit_btn.TabIndex = 13;
+            this.quit_btn.Text = "&Quit";
+            this.quit_btn.UseVisualStyleBackColor = true;
+            this.quit_btn.Click += new System.EventHandler(this.quit_btn_Click);
+            // 
+            // start_Btn
+            // 
+            this.start_Btn.Location = new System.Drawing.Point(800, 436);
+            this.start_Btn.Name = "start_Btn";
+            this.start_Btn.Size = new System.Drawing.Size(75, 23);
+            this.start_Btn.TabIndex = 1;
+            this.start_Btn.Text = "&Start";
+            this.start_Btn.UseVisualStyleBackColor = true;
+            this.start_Btn.Click += new System.EventHandler(this.start_Btn_Click);
+            // 
+            // result_richTextBox
+            // 
+            this.result_richTextBox.Location = new System.Drawing.Point(1, 2);
+            this.result_richTextBox.Name = "result_richTextBox";
+            this.result_richTextBox.Size = new System.Drawing.Size(960, 430);
+            this.result_richTextBox.TabIndex = 12;
+            this.result_richTextBox.Text = "Progress:";
+            this.result_richTextBox.TextChanged += new System.EventHandler(this.result_richTextBox_TextChanged);
+            // 
+            // page_no_label
+            // 
+            this.page_no_label.AutoSize = true;
+            this.page_no_label.Location = new System.Drawing.Point(303, 441);
+            this.page_no_label.Name = "page_no_label";
+            this.page_no_label.Size = new System.Drawing.Size(67, 13);
+            this.page_no_label.TabIndex = 21;
+            this.page_no_label.Text = "700 (Approx)";
+            this.page_no_label.Click += new System.EventHandler(this.page_no_label_Click);
+            // 
+            // JobsInDubai
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 461);
+            this.Controls.Add(this.page_no_label);
+            this.Controls.Add(this.nextPage_label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.url_label);
+            this.Controls.Add(this.url_textBox);
+            this.Controls.Add(this.open_btn);
+            this.Controls.Add(this.quit_btn);
+            this.Controls.Add(this.start_Btn);
+            this.Controls.Add(this.result_richTextBox);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(978, 500);
+            this.MinimumSize = new System.Drawing.Size(978, 500);
+            this.Name = "JobsInDubai";
+            this.Text = "JobsInDubai.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nextPage_label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label url_label;
+        private System.Windows.Forms.TextBox url_textBox;
+        private System.Windows.Forms.Button open_btn;
+        private System.Windows.Forms.Button quit_btn;
+        private System.Windows.Forms.Button start_Btn;
+        private System.Windows.Forms.RichTextBox result_richTextBox;
+        private System.Windows.Forms.Label page_no_label;
+    }
+}
+
---
+++ JobsInDubai.com/JobsInDubai.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+
+/*
+Require dependencies
+Name:	ChromeDriver
+ID:	WebDriver.ChromeDriver.win32
+URL:	https://www.nuget.org/packages/WebDriver.ChromeDriver.win32/2.14.0
+
+Name:	Selenium
+ID:	Selenium.WebDriver
+URL:	https://www.nuget.org/packages/Selenium.WebDriver/2.45.0
+*/
+
+/*well I am including over here */
+using OpenQA.Selenium;
+/* Chrome will do Perfect Job so adding it. :) */
+using OpenQA.Selenium.Chrome;
+/* Some File Writing */
+using System.IO;
+/* Xpath Lib */
+using System.Xml.XPath;
+
+namespace JobsInDubai.com
+{
+    public partial class JobsInDubai : Form
+    {
+
+        //user desktop path to store display files
+        string path_Desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        //https://www.jobsindubai.com/jobs.asp			678 * 10 Jobs
+        //https://www.jobsindubai.com/job_list.asp?page=663&lstIndustryID=0&txtKeyword=&isSearch=False
+        //http://web.archive.org/web/*/https://www.jobsindubai.com/jobs.asp
+        public String default_Url = "https://www.jobsindubai.com";
+
+        public void JobsInDubai_Scraper(string default_Url)
+        {
+            var job_title = "";
+            var job_company = "";
+            var job_desc = "";
+
+            var job_location = "";
+            var job_nationality = "";
+            var job_experience = "";
+            var job_keyskils = "";
+            var job_function = "";
+            var job_region = "United Arab Emirates";
+            var job_industry = "";
+            var job_date = "";
+            var job_education = "";
+            var next_page_class = 0;
+            var page_number = 1;
+            nextPage_label.Text = "Yes";
+
+            /* initializing chrome driver */
+            var chrome_Driver = ChromeDriverService.CreateDefaultService();
+            //no need to show disturbing command window so set it hidden
+            chrome_Driver.HideCommandPromptWindow = true;
+
+            var main_page_chrome_Driver_Obj = new ChromeDriver(chrome_Driver, new ChromeOptions());
+
+            /* create a csv file for output*/
+            try
+            {
+                File.WriteAllText(Path.Combine(path_Desktop, "JobsInDubai_Scrapped_DataSet_allTypesJobs.csv"), "Job Title, Company, Description, Location, Experience,  Keyskills, Education, Region, Industry, Date\r\n");
+            }
+            catch (Exception ex)
+            {
+
+                //incase of file is open or readonly or no permission show message
+                result_richTextBox.AppendText(ex.StackTrace);
+                MessageBox.Show("Cannot write to file !");
+            }
+
+            do
+            {
+
+                if (next_page_class == 0) { nextPage_label.Text = "No"; }
+                //this.page_no_label.Text = page_number.ToString();
+
+                main_page_chrome_Driver_Obj.Navigate().GoToUrl(default_Url + "/job_list.asp?page=" + page_number + "&lstIndustryID=0&txtKeyword=&isSearch=False");
+
+                // click the Detail View Link to show extra information for jobs
+                var click_Detail_View = main_page_chrome_Driver_Obj.FindElement(By.XPath("//*/div[@id='divShowAll']/a"));
+                if (click_Detail_View.Text == "Detail View") { click_Detail_View.Click(); }
+
+                //count number of jobs on main page max 10 jobs
+                var var_job_count = main_page_chrome_Driver_Obj.FindElementsByXPath("//*[@class='even']");
+                var var_job_des = main_page_chrome_Driver_Obj.FindElementsByXPath("//*/table[@class='expand']");
+
+                for (int job_index = 0; job_index < var_job_count.Count(); job_index++)
+                {
+                    job_title = var_job_count[job_index].FindElement(By.XPath("./td[2]")).Text;
+                    job_title = cleanString(job_title);
+
+                    job_company = var_job_des[job_index].FindElement(By.XPath("./tbody/tr[2]/td[2]/span[1]")).Text;
+                    job_company = cleanString(job_company);
+
+                    job_location = var_job_des[job_index].FindElement(By.XPath("./tbody/tr[2]/td[2]/span[2]")).Text;
+                    job_location = cleanString(job_location);
+
+                    var job_full_desc_raw = var_job_des[job_index].FindElement(By.XPath("./tbody/tr[4]/td")).Text;
+                    var job_full_desc = job_full_desc_raw.Replace(System.Environment.NewLine, " ");
+
+                    job_experience = ParseBetween(job_full_desc, "Experience :", "Years");
+                    job_experience = cleanString(job_experience);
+                    if (job_experience == string.Empty) { job_experience = "Not Required"; } else { job_experience = job_experience + " Years"; }
+
+                    job_education = ParseBetween(job_full_desc, "Education :", "Experience :");
+                    job_education = cleanString(job_education);
+                    if (job_education == string.Empty) { job_education = "NULL"; }
+
+                    //All this headach to extract Skills
+                    
+                    job_keyskils = ParseBetween(job_full_desc, "Skills :", "Description :");
+                    job_keyskils = cleanString(job_keyskils);
+                    if (job_keyskils == string.Empty)
+                    {
+                        job_keyskils = ParseBetween(job_full_desc, "Skills :", "Responsibilities :");
+                        job_keyskils = cleanString(job_keyskils);
+                        if (job_keyskils == string.Empty)
+                        {
+                            job_keyskils = ParseBetween(job_full_desc, "Responsibilities :", "Description :");
+                            job_keyskils = cleanString(job_keyskils);
+                            if (job_keyskils == string.Empty) { job_keyskils = "NULL"; }
+                        }
+                    }
+
+                    //End Skills
+
+                    job_industry = var_job_count[job_index].FindElement(By.XPath("./td[3]")).Text;
+                    job_industry = cleanString(job_industry);
+
+                    job_date = var_job_count[job_index].FindElement(By.XPath("./td[4]")).Text;
+                    job_date = cleanString(job_date);
+                    job_date = timeStampCnversion(job_date);
+
+                    job_desc = cleanString(job_full_desc);
+                    //+++++++++++++ values Scraper Region ENd +++++++++++++++++++++++++
+
+                    //Dispaly Output and write data to CSV file
+                    //Job Title, Company, Description, Location, Experience,  Keyskills, Region, Industry, Date\r\n
+                    result_richTextBox.AppendText("\nJob Title: " + job_title.ToString().Trim() + "\nCompany: " + job_company.ToString().Trim() + "\nDescription: " + job_desc.ToString().Trim() + "\nLocation: " + job_location.ToString().Trim() + "\nExperience: " + job_experience.ToString().Trim() + "\nKeyskills: " + job_keyskils.ToString().Trim() + "\nEducation: " + job_education.ToString().Trim() + "\nRegion: " + job_region.ToString().Trim() + "\nIndustry: " + job_industry.ToString().Trim() + "\nDate: " + job_date.ToString().Trim() + "\r.....................................................................................................\n");
+                    File.AppendAllText(Path.Combine(path_Desktop, "JobsInDubai_Scrapped_DataSet_allTypesJobs.csv"), job_title.ToString().Trim().Replace(",", "") + "," + job_company.ToString().Trim().Replace(",", "") + "," + job_desc.ToString().Trim().Replace(",", "") + "," + job_location.ToString().Trim().Replace(",", "") + "," + job_experience.ToString().Trim().Replace(",", "") + "," + job_keyskils.ToString().Trim().Replace(",", "") + "," + job_education.ToString().Trim().Replace(",", "") + "," + job_region.ToString().Trim().Replace(",", "") + "," + job_industry.ToString().Trim().Replace(",", "") + "," + job_date.ToString().Trim().Replace(",", "") + "\r\n");
+
+                }
+
+                //these three line to loop main pages
+                var var_next_page_class = main_page_chrome_Driver_Obj.FindElementsByXPath("//*[@class='next']");
+                next_page_class = var_next_page_class.Count();
+                page_number++;
+            } while (next_page_class == 1);
+            main_page_chrome_Driver_Obj.Quit();
+            main_page_chrome_Driver_Obj.Dispose();
+        }
+
+        public JobsInDubai()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Btn_Click(object sender, EventArgs e)
+        {
+            JobsInDubai_Scraper(default_Url);
+        }
+
+        private void open_btn_Click(object sender, EventArgs e)
+        {
+            Process.Start(path_Desktop);
+        }
+
+        private void quit_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void nextPage_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_richTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public string ParseBetween(string Subject, string Start, string End)
+        {
+            return Regex.Match(Subject, Regex.Replace(Start, @"[][{}()*+?.\\^$|]", @"\$0") + @"\s*(((?!" + Regex.Replace(Start, @"[][{}()*+?.\\^$|]", @"\$0") + @"|" + Regex.Replace(End, @"[][{}()*+?.\\^$|]", @"\$0") + @").)+)\s*" + Regex.Replace(End, @"[][{}()*+?.\\^$|]", @"\$0"), RegexOptions.IgnoreCase).Value.Replace(Start, "").Replace(End, "");
+        }
+
+        //Date to Time Stamp Converter Function
+        public string timeStampCnversion(string string_date)
+        {
+            //standard date timeStampCnversion format is 6/14/2015
+            // current website date format is 2/22/2013
+            string s = string_date;
+            string[] values = s.Split('/');
+            string str_month = values[0].Trim();
+            string str_day = values[1].Trim();
+            string str_year = values[2].Trim();
+            int int_year = int.Parse(str_year);
+            int int_month = int.Parse(str_month);
+            int int_day = int.Parse(str_day);
+
+
+            DateTime date = new DateTime(int_year, int_month, int_day, 12, 0, 0, 0);
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan span = (date - epoch);
+            double unixTime = span.TotalSeconds;
+            return unixTime.ToString();
+        }
+
+        public string cleanString(string str)
+        {
+            return str.Trim(new Char[] { ' ', '\r', '\n' });
+        }
+
+        private void page_no_label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
